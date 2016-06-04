faf837a9514323a675a55d076aff443862bace39
Theo Martin <theo.martin22@icloud.com>
2016-06-04 16:59:49 +0200

    Create template portfolio 2

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Portfolio 2</title>
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="design-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h1>Théo Martin</h1>
+				</div>
+				<div class="col-md-6">
+					<nav id="nav-principal">
+						<ul>
+							<li><a href="#">Présentation</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include "header.php"; ?>
+<div class="content">
+	<div class="presentation">
+		<div class="container">
+			<h2>Title</h2>
+			<p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat voluptatem culpa, aliquam odit explicabo unde. Dicta aut minus, odio corporis rem perspiciatis aspernatur commodi assumenda hic architecto eius repellendus reprehenderit.</span>
+			<span>Accusantium provident laboriosam perspiciatis nesciunt, magni sint maxime, fugit totam voluptas distinctio, enim libero voluptatum corporis iure alias voluptatibus amet dolore tenetur ad recusandae cum. Repudiandae quibusdam impedit rerum facere!</span>
+			<span>Aut, autem? Quisquam consequuntur veritatis corporis, voluptatibus, deserunt exercitationem quas nihil! Velit debitis tempore nisi atque reiciendis corrupti asperiores corporis, a ad voluptatibus aspernatur pariatur ea unde impedit animi quia.</span>
+			<span>Animi eligendi sunt odio vitae facere, voluptates, nulla temporibus accusamus labore recusandae tempore architecto aspernatur error at ex dignissimos corporis necessitatibus eum libero! Excepturi magni, sapiente corporis. Nihil, quasi cumque.</span></p>
+		</div>
+	</div>
+</div>
+<?php include "footer.php"; ?>
